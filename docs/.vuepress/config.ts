96bcd3861915b02e1aa2c069b57ac3a3cacce2ff
mon_sat <tnk@daijiten.info>
2022-01-06 14:28:58 +0900

    feat: add plugin netabare-switch

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -74,6 +74,9 @@ export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
     ['vuepress-plugin-use-pages', {
       startsWith: '/a/',
     }],
+    // @TODO: yarn link を使用するともっと楽に紐付けられる
+    // [path.resolve(__dirname, './plugin/vuepress-plugin-netabare-switch')],
+    ['vuepress-plugin-netabare-switch'],
     ['@vuepress/plugin-google-analytics', { id: 'G-83SWW0DJYQ' }],
   ],
   shouldPrefetch: true,
