905d2c64ca1148aeaf7807a26e838c5693e98418
mon_sat <tnk@daijiten.info>
2022-06-18 18:40:08 +0900

    feat: update for VuePress 2.0.0-beta.40

--- docs/.vuepress/theme/index.ts
+++ docs/.vuepress/theme/index.ts
@@ -1,12 +1,14 @@
-import type { ThemeObject } from '@vuepress/core'
+import { defaultTheme } from 'vuepress'
+import type { Theme, ThemeObject } from '@vuepress/core'
 import { path } from '@vuepress/utils'
 
-const childDefaultTheme: ThemeObject = {
+export const childDefaultTheme: Theme = (): ThemeObject => ({
   name: 'vuepress-theme-default-child',
-  extends: '@vuepress/theme-default',
+  extends: defaultTheme,
   layouts: {
     Layout: path.resolve(__dirname, 'layouts/Layout.vue'),
+    logo: '/images/kohji_vector_white.png',
   },
-}
+})
 
 export default childDefaultTheme
